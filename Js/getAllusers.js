573a3ec2dbf728cb829ab1fcbf679f37fe297661
hp <emmanueldusengimana83@gmail.com>
2023-11-04 11:34:01 +0200

    jh

--- Js/getAllusers.js
+++ Js/getAllusers.js
@@ -31,9 +31,48 @@ fetch(`https://rich-tan-dhole-cap.cyclic.app/rice/user/get`,postman)
         <td>${user.password}</td>
         <td>${user.role}</td>
         <td>${user.signedAt}</td>
-        <td><button class="update" style="color:green">Update</button></td>
-        <td><button class="delete" style="color:red">Delete</button></td>
+        <td><button class="update" id="${user._id}" style="color:green">Update</button></td>
+        <td><button class="delete" id="${user._id}" style="color:red">Delete</button></td>
         </tr>
         `
     })
+
+    const buttoselector =document.querySelectorAll(".update")
+    const formla=document.querySelector(".form-updata")
+    for(let i=0;i<buttoselector.length;i++){
+        buttoselector[i].onclick=function(){
+            formla.style.display="block"
+
+            let tr=this.parentElement.parentElement
+            let td=tr.getElementsByTagName("td")
+            let id=buttoselector[i].getAttribute("id")
+            role.value=td[5].innerHTML
+
+            formla.addEventListener("submit",()=>{
+                const datas={
+                    role:role.value
+                }
+                const postman={
+                    method:"PATCH",
+                    headers:{
+                        "Content-Type":"application/json"
+                    },
+                    body:JSON.stringify(datas)
+                }
+
+                fetch(`https://rich-tan-dhole-cap.cyclic.app/rice/user/update/${id}`,postman)
+
+                .then((resp)=>{
+                    return resp.json
+                })
+                .then((datas)=>{
+                    alert(datas.message) 
+                })
+                .catch((err)=>{
+                    alert(err) 
+                    
+                })
+            }) 
+        }
+    }
 })
